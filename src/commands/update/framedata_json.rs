3edc037c9b41a3038a646961612b5638042e7a28
eda3 <gerorin1010@gmail.com>
2024-04-04 07:49:40 +0900

    cargo fmt

--- src/commands/update/framedata_json.rs
+++ src/commands/update/framedata_json.rs
@@ -1,17 +1,17 @@
-use std::io::Write;
+use crate::{MoveInfo, CHARS};
 use serde::Deserialize;
-use crate::{CHARS, MoveInfo};
 use std::fs::File;
+use std::io::Write;
 extern crate ureq;
 
 #[derive(Deserialize, Debug)]
 struct Response {
-    cargoquery: Vec<Data>
+    cargoquery: Vec<Data>,
 }
 
 #[derive(Deserialize, Debug)]
 struct Data {
-    title: Title
+    title: Title,
 }
 
 #[derive(Deserialize, Debug)]
@@ -35,8 +35,11 @@ struct Title {
     counter: Option<String>,
 }
 
-pub async fn frames_to_json(mut char_page_response_json: String, mut file: &File, char_count: usize) {
-
+pub async fn frames_to_json(
+    mut char_page_response_json: String,
+    mut file: &File,
+    char_count: usize,
+) {
     char_page_response_json = char_page_response_json.replace(r#"&lt;br&gt;"#, ", ");
     char_page_response_json = char_page_response_json.replace(r#"&lt;br/&gt;"#, ", ");
     // Ino low profile
@@ -45,100 +48,181 @@ pub async fn frames_to_json(mut char_page_response_json: String, mut file: &File
     let mut moves_info: Response = serde_json::from_str(&char_page_response_json).unwrap();
 
     for x in 0..moves_info.cargoquery.len() {
-        
         // Replacing None values with a generic '-'
-        if moves_info.cargoquery[x].title.input.is_none(){
+        if moves_info.cargoquery[x].title.input.is_none() {
             moves_info.cargoquery[x].title.input = Some("-".to_string());
-        }
-        else{
+        } else {
             // Skips finish blow for sol
-            if *moves_info.cargoquery[x].title.input.as_ref().unwrap() == "j.XX during Homing Jump" {
+            if *moves_info.cargoquery[x].title.input.as_ref().unwrap() == "j.XX during Homing Jump"
+            {
                 continue;
             }
         }
-        if moves_info.cargoquery[x].title.name.is_none(){
-            moves_info.cargoquery[x].title.name = Some(moves_info.cargoquery[x].title.input.as_ref().unwrap().to_string());
-        }
-        else {
-            // Skips dash cancel entry, ino hoverdash chipp escape zato flight and finish blow 
-            if *moves_info.cargoquery[x].title.name.as_ref().unwrap() == "Dash Cancel" || 
-            *moves_info.cargoquery[x].title.name.as_ref().unwrap() == "Hoverdash" ||
-            *moves_info.cargoquery[x].title.name.as_ref().unwrap() == "Finish Blow" ||
-            *moves_info.cargoquery[x].title.name.as_ref().unwrap() == "Flight" ||
-            *moves_info.cargoquery[x].title.name.as_ref().unwrap() == "Escape" {
+        if moves_info.cargoquery[x].title.name.is_none() {
+            moves_info.cargoquery[x].title.name = Some(
+                moves_info.cargoquery[x]
+                    .title
+                    .input
+                    .as_ref()
+                    .unwrap()
+                    .to_string(),
+            );
+        } else {
+            // Skips dash cancel entry, ino hoverdash chipp escape zato flight and finish blow
+            if *moves_info.cargoquery[x].title.name.as_ref().unwrap() == "Dash Cancel"
+                || *moves_info.cargoquery[x].title.name.as_ref().unwrap() == "Hoverdash"
+                || *moves_info.cargoquery[x].title.name.as_ref().unwrap() == "Finish Blow"
+                || *moves_info.cargoquery[x].title.name.as_ref().unwrap() == "Flight"
+                || *moves_info.cargoquery[x].title.name.as_ref().unwrap() == "Escape"
+            {
                 continue;
             }
         }
-        if moves_info.cargoquery[x].title.damage.is_none(){
+        if moves_info.cargoquery[x].title.damage.is_none() {
             moves_info.cargoquery[x].title.damage = Some("-".to_string());
         }
-        if moves_info.cargoquery[x].title.guard.is_none(){
+        if moves_info.cargoquery[x].title.guard.is_none() {
             moves_info.cargoquery[x].title.guard = Some("-".to_string());
         }
-        if moves_info.cargoquery[x].title.invuln.is_none(){
+        if moves_info.cargoquery[x].title.invuln.is_none() {
             moves_info.cargoquery[x].title.invuln = Some("-".to_string());
         }
-        if moves_info.cargoquery[x].title.startup.is_none(){
+        if moves_info.cargoquery[x].title.startup.is_none() {
             moves_info.cargoquery[x].title.startup = Some("-".to_string());
         }
-        if moves_info.cargoquery[x].title.active.is_none(){
+        if moves_info.cargoquery[x].title.active.is_none() {
             moves_info.cargoquery[x].title.active = Some("-".to_string());
         }
-        if moves_info.cargoquery[x].title.recovery.is_none(){
+        if moves_info.cargoquery[x].title.recovery.is_none() {
             moves_info.cargoquery[x].title.recovery = Some("-".to_string());
         }
-        if moves_info.cargoquery[x].title.hit.is_none(){
+        if moves_info.cargoquery[x].title.hit.is_none() {
             moves_info.cargoquery[x].title.hit = Some("-".to_string());
         }
-        if moves_info.cargoquery[x].title.block.is_none(){
+        if moves_info.cargoquery[x].title.block.is_none() {
             moves_info.cargoquery[x].title.block = Some("-".to_string());
         }
-        if moves_info.cargoquery[x].title.level.is_none(){
+        if moves_info.cargoquery[x].title.level.is_none() {
             moves_info.cargoquery[x].title.level = Some("-".to_string());
         }
-        if moves_info.cargoquery[x].title.riscgain.is_none(){
+        if moves_info.cargoquery[x].title.riscgain.is_none() {
             moves_info.cargoquery[x].title.riscgain = Some("-".to_string());
         }
-        if moves_info.cargoquery[x].title.prorate.is_none(){
+        if moves_info.cargoquery[x].title.prorate.is_none() {
             moves_info.cargoquery[x].title.prorate = Some("-".to_string());
         }
-        if moves_info.cargoquery[x].title.counter.is_none(){
+        if moves_info.cargoquery[x].title.counter.is_none() {
             moves_info.cargoquery[x].title.counter = Some("-".to_string());
         }
 
         // Serializing frame data
         let processed_moves_info = serde_json::to_string(&MoveInfo {
-            input: moves_info.cargoquery[x].title.input.as_ref().unwrap().to_string(),
-            name: moves_info.cargoquery[x].title.name.as_ref().unwrap().to_string(),
-            damage: moves_info.cargoquery[x].title.damage.as_ref().unwrap().to_string(),
-            guard: moves_info.cargoquery[x].title.guard.as_ref().unwrap().to_string(),
-            invincibility: moves_info.cargoquery[x].title.invuln.as_ref().unwrap().to_string(),
-            startup: moves_info.cargoquery[x].title.startup.as_ref().unwrap().to_string(),
-            active: moves_info.cargoquery[x].title.active.as_ref().unwrap().to_string(),
-            recovery: moves_info.cargoquery[x].title.recovery.as_ref().unwrap().to_string(),
-            hit: moves_info.cargoquery[x].title.hit.as_ref().unwrap().to_string(),
-            block: moves_info.cargoquery[x].title.block.as_ref().unwrap().to_string(),
-            level: moves_info.cargoquery[x].title.level.as_ref().unwrap().to_string(),
-            riscgain: moves_info.cargoquery[x].title.riscgain.as_ref().unwrap().to_string(),
-            scaling: moves_info.cargoquery[x].title.prorate.as_ref().unwrap().to_string(),
-            counter: moves_info.cargoquery[x].title.counter.as_ref().unwrap().to_string(),
-        }).unwrap();
-        
+            input: moves_info.cargoquery[x]
+                .title
+                .input
+                .as_ref()
+                .unwrap()
+                .to_string(),
+            name: moves_info.cargoquery[x]
+                .title
+                .name
+                .as_ref()
+                .unwrap()
+                .to_string(),
+            damage: moves_info.cargoquery[x]
+                .title
+                .damage
+                .as_ref()
+                .unwrap()
+                .to_string(),
+            guard: moves_info.cargoquery[x]
+                .title
+                .guard
+                .as_ref()
+                .unwrap()
+                .to_string(),
+            invincibility: moves_info.cargoquery[x]
+                .title
+                .invuln
+                .as_ref()
+                .unwrap()
+                .to_string(),
+            startup: moves_info.cargoquery[x]
+                .title
+                .startup
+                .as_ref()
+                .unwrap()
+                .to_string(),
+            active: moves_info.cargoquery[x]
+                .title
+                .active
+                .as_ref()
+                .unwrap()
+                .to_string(),
+            recovery: moves_info.cargoquery[x]
+                .title
+                .recovery
+                .as_ref()
+                .unwrap()
+                .to_string(),
+            hit: moves_info.cargoquery[x]
+                .title
+                .hit
+                .as_ref()
+                .unwrap()
+                .to_string(),
+            block: moves_info.cargoquery[x]
+                .title
+                .block
+                .as_ref()
+                .unwrap()
+                .to_string(),
+            level: moves_info.cargoquery[x]
+                .title
+                .level
+                .as_ref()
+                .unwrap()
+                .to_string(),
+            riscgain: moves_info.cargoquery[x]
+                .title
+                .riscgain
+                .as_ref()
+                .unwrap()
+                .to_string(),
+            scaling: moves_info.cargoquery[x]
+                .title
+                .prorate
+                .as_ref()
+                .unwrap()
+                .to_string(),
+            counter: moves_info.cargoquery[x]
+                .title
+                .counter
+                .as_ref()
+                .unwrap()
+                .to_string(),
+        })
+        .unwrap();
+
         write!(file, "{}", processed_moves_info)
-        .expect(&("\nFailed to serialize '".to_owned() + CHARS[char_count]+ ".json'."));
-        
-        // Skip writting comma/tab if next and last iteration 
+            .expect(&("\nFailed to serialize '".to_owned() + CHARS[char_count] + ".json'."));
+
+        // Skip writting comma/tab if next and last iteration
         // contains 'finish blow' in last the input field
-        if x == moves_info.cargoquery.len() -2 &&
-        *moves_info.cargoquery[x+1].title.input.as_ref().unwrap() == "j.XX during Homing Jump" {
+        if x == moves_info.cargoquery.len() - 2
+            && *moves_info.cargoquery[x + 1].title.input.as_ref().unwrap()
+                == "j.XX during Homing Jump"
+        {
             continue;
-        }
-        else if x != moves_info.cargoquery.len() - 1 {         
+        } else if x != moves_info.cargoquery.len() - 1 {
             // Adding comma/tab
             // file.write(b",\n\t")
             //     .expect(&("\nFailed to write ',\\n\\t' while serializing '".to_owned() + CHARS[char_count]+ ".json'."));
-            (&mut file).write_all(b",\n\t")
-                .expect(&("\nFailed to write ',\\n\\t' while serializing '".to_owned() + CHARS[char_count]+ ".json'."));
-        } 
+            (&mut file).write_all(b",\n\t").expect(
+                &("\nFailed to write ',\\n\\t' while serializing '".to_owned()
+                    + CHARS[char_count]
+                    + ".json'."),
+            );
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-mod commands;
 mod check;
+mod commands;
 mod find;
 mod ran;
 use colored::Colorize;
 use commands::*;
 use poise::serenity_prelude as serenity;
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 use std::time::Duration;
 
 // Types used by all command functions
@@ -17,7 +17,7 @@ pub struct Data {}
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct CharInfo {
-    page: String
+    page: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -83,24 +83,40 @@ pub const CHARS: [&str; 26] = [
     "Sin_Kiske",
     "Sol_Badguy",
     "Testament",
-    "Zato-1"
+    "Zato-1",
 ];
 
-const IMAGE_DEFAULT: &str = "https://raw.githubusercontent.com/yakiimoninja/baiken/main/data/images/no_image.png";
-const HITBOX_DEFAULT: &str = "https://raw.githubusercontent.com/yakiimoninja/baiken/main/data/images/no_hitbox.png";
+const IMAGE_DEFAULT: &str =
+    "https://raw.githubusercontent.com/yakiimoninja/baiken/main/data/images/no_image.png";
+const HITBOX_DEFAULT: &str =
+    "https://raw.githubusercontent.com/yakiimoninja/baiken/main/data/images/no_hitbox.png";
 
 async fn on_error(error: poise::FrameworkError<'_, Data, Error>) {
     // This is our custom error handler
     // They are many errors that can occur, so we only handle the ones we want to customize
     // and forward the rest to the default handler
     match error {
-        poise::FrameworkError::Setup { error, .. } => panic!("{}", ("Failed to start bot: ".to_owned() + &error.to_string() + ".").red()),
+        poise::FrameworkError::Setup { error, .. } => panic!(
+            "{}",
+            ("Failed to start bot: ".to_owned() + &error.to_string() + ".").red()
+        ),
         poise::FrameworkError::Command { error, ctx } => {
-            println!("{}", ("Error in command `".to_owned() + &ctx.command().name + "`: " + &error.to_string() + ".").red());
+            println!(
+                "{}",
+                ("Error in command `".to_owned()
+                    + &ctx.command().name
+                    + "`: "
+                    + &error.to_string()
+                    + ".")
+                    .red()
+            );
         }
         error => {
             if let Err(e) = poise::builtins::on_error(error).await {
-                println!("{}", ("Error while handling error: ".to_owned() + &e.to_string() + ".").red())
+                println!(
+                    "{}",
+                    ("Error while handling error: ".to_owned() + &e.to_string() + ".").red()
+                )
             }
         }
     }
@@ -108,7 +124,6 @@ async fn on_error(error: poise::FrameworkError<'_, Data, Error>) {
 
 #[tokio::main]
 async fn main() {
-    
     println!("メイン！");
 
     // Running initial checks
@@ -129,31 +144,36 @@ async fn main() {
             nicknames::nicknames(),
             moves::moves(),
             register::register(),
-            update::update(),        
+            update::update(),
         ],
         prefix_options: poise::PrefixFrameworkOptions {
-             prefix: Some("!".into()),
-             edit_tracker: Some(poise::EditTracker::for_timespan(Duration::from_secs(3600))),
-             additional_prefixes: vec![
-                 poise::Prefix::Literal("b."),
-             ],
-             ..Default::default()
-         },
-        
+            prefix: Some("!".into()),
+            edit_tracker: Some(poise::EditTracker::for_timespan(Duration::from_secs(3600))),
+            additional_prefixes: vec![poise::Prefix::Literal("b.")],
+            ..Default::default()
+        },
+
         // The global error handler for all error cases that may occur
         on_error: |error| Box::pin(on_error(error)),
-        
+
         // This code is run before every command
         pre_command: |ctx| {
             Box::pin(async move {
-                println!("{}", ("\nExecuting command ".to_owned() + &ctx.command().qualified_name + "...").cyan());
+                println!(
+                    "{}",
+                    ("\nExecuting command ".to_owned() + &ctx.command().qualified_name + "...")
+                        .cyan()
+                );
             })
         },
 
         // This code is run after a command if it was successful (returned Ok)
         post_command: |ctx| {
             Box::pin(async move {
-                println!("{}", ("Executed command ".to_owned() + &ctx.command().qualified_name + "!").cyan());
+                println!(
+                    "{}",
+                    ("Executed command ".to_owned() + &ctx.command().qualified_name + "!").cyan()
+                );
             })
         },
 
@@ -166,7 +186,7 @@ async fn main() {
                 Ok(true)
             })
         }),
-        
+
         // // Uncomment for debugging
         // listener: |_ctx, event, _framework, _data| {
         //     Box::pin(async move {
