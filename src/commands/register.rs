d12c78d590a5ebb55b5ef3db501377124c936c84
eda3 <gerorin1010@gmail.com>
2025-02-20 05:40:56 +0900

    日本語コメント追加

--- src/commands/register.rs
+++ src/commands/register.rs
@@ -1,8 +1,16 @@
+//! # register.rs
+//!  
+//! アプリケーションコマンド登録・解除モジュール  
+//! Discordコマンド /register 実装モジュール  
+//! ギルドまたはグローバルでアプリケーションコマンドを登録・解除する処理を提供  
+//! このコマンドは、Botの所有者のみ実行可能
+
 use crate::{Context, Error};
 
-/// Registers or unregisters application commands in this guild or globally. Owners only!
+/// アプリケーションコマンドの登録・解除処理  
 #[poise::command(prefix_command, slash_command, hide_in_help, owners_only)]
 pub async fn register(ctx: Context<'_>) -> Result<(), Error> {
+    // アプリケーションコマンドの登録・解除用ボタンを表示する処理
     poise::builtins::register_application_commands_buttons(ctx).await?;
 
     Ok(())
