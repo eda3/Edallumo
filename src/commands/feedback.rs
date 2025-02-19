a06a73e86f18de0263f1c3f74d6fab47f78ca6b1
eda3 <gerorin1010@gmail.com>
2025-02-19 22:32:11 +0900

    コメント追加

--- src/commands/feedback.rs
+++ src/commands/feedback.rs
@@ -2,24 +2,29 @@ use crate::{Context, Error};
 use colored::Colorize;
 use std::{fs::OpenOptions, io::Write};
 
-/// Gives feedback or requests something from the dev.
+/// フィードバック送信処理
+/// 開発者宛フィードバック登録
 #[poise::command(prefix_command, slash_command, aliases("r"))]
 pub async fn feedback(
     ctx: Context<'_>,
     #[description = "Message for the dev."] text: String,
 ) -> Result<(), Error> {
-    // Creating character json file
+    // ファイルオープン処理
     let mut file = OpenOptions::new()
         .create(true)
         .append(true)
         .open("request.txt")
         .expect("\nFailed to open 'request.txt' file.");
 
+    // テキスト整形処理
     let new_text = text.to_owned() + "\n\n";
 
+    // ファイル書き込み処理
     write!(file, "{}", new_text).expect("\nFailed to write to 'request.txt'");
 
+    // ログ出力処理
     println!("{}", "Done writting to 'request.txt'".yellow());
+    // 送信メッセージ出力処理
     ctx.say("Submitted successfully!").await?;
 
     Ok(())
