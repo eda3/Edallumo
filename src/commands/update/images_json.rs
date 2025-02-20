b00b59f0aab27923e092641e4d342e14fc6985e9
eda3 <gerorin1010@gmail.com>
2025-02-20 21:37:32 +0900

    不要な「Heat Tackle」置換の重複行を削除

--- src/commands/update/images_json.rs
+++ src/commands/update/images_json.rs
@@ -873,7 +873,6 @@ pub async fn images_to_json(mut chara_response_json: String, mut file: &File, ch
         .replace(r#""[4]6H P""#, r#""[4]6HS P""#) // ポチョムキン
         .replace(r#""[4]6H""#, r#""[4]6HS""#) // ポチョムキン
         .replace(r#""Heat Tackle""#, r#""ヒートタックル""#) // ポチョムキン
-        .replace(r#""Heat Tackle""#, r#""ヒートタックル""#) // ポチョムキン
         .replace(r#""F.D.B. (Charged)""#, r#""溜めF.D.B.""#) // ポチョムキン
         // シン
         .replace(r#""Hoof Stomp""#, r#""フーフスタンプ""#)
