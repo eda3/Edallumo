d7df661fa1e3fc87744ceaf19fc3bf21aef6b213
eda3 <eda3@users.noreply.github.com>
2025-03-02 10:10:09 +0000

    deploy: e51d6648efaba6eaf91325449a0f5627bbb1da87

--- baiken/common/preprocess/sidebar-items.js
+++ baiken/common/preprocess/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["preprocess_json"]};
+window.SIDEBAR_ITEMS = {"fn":["preprocess_json"],"struct":["REPLACEMENT_MAP","RE_C_S","RE_F_S","RE_HKD_MOVE","RE_J_MOVE","RE_KD_MOVE"]};
